49c6b5b772db7865ce6449693a59018dbc9a4d71
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-12-14 12:09:42 +0900

    oauthを修正。

--- dromozoa/http/oauth.lua
+++ dromozoa/http/oauth.lua
@@ -16,7 +16,12 @@
 -- along with dromozoa-http.  If not, see <http://www.gnu.org/licenses/>.
 
 local base64 = require "dromozoa.commons.base64"
+local clone = require "dromozoa.commons.clone"
 local random_bytes = require "dromozoa.commons.random_bytes"
+local sequence = require "dromozoa.commons.sequence"
+local sequence_writer = require "dromozoa.commons.sequence_writer"
+local sha1 = require "dromozoa.commons.sha1"
+local uri = require "dromozoa.commons.uri"
 
 local class = {}
 
@@ -39,12 +44,107 @@ function class:reset(oauth_timestamp, oauth_nonce)
   return self
 end
 
+function class:build(request)
+  request:build()
+  if request.oauth == nil then
+    request.oauth = {}
+  end
+  return self
+end
+
+function class:make_parameter_string(request)
+  local this = request.oauth
+  local oauth_params = sequence()
+    :push({ "oauth_consumer_key", self.oauth_consumer_key })
+    :push({ "oauth_nonce", self.oauth_nonce })
+    :push({ "oauth_signature_method", "HMAC-SHA1" })
+    :push({ "oauth_timestamp", self.oauth_timestamp })
+    :push({ "oauth_token", self.oauth_token })
+    :push({ "oauth_version", "1.0" })
+  for param in request.uri.query.params:each() do
+    oauth_params:push({ param[1], param[2] })
+  end
+  for param in request.params:each() do
+    oauth_params:push({ param[1], param[2] })
+  end
+  for param in oauth_params:each() do
+    param[1] = uri.encode(param[1])
+    param[2] = uri.encode(param[2])
+  end
+  oauth_params:sort(function (a, b) return a[1] < b[1] end)
+  this.oauth_params = oauth_params
+
+  local out = sequence_writer()
+  local first = true
+  for param in oauth_params:each() do
+    if first then
+      first = false
+    else
+      out:write("&")
+    end
+    out:write(param[1], "=", param[2])
+  end
+  this.parameter_string = out:concat()
+  return self
+end
+
+function class:make_signature_base_string(request)
+  local this = request.oauth
+
+  local url = clone(request.uri)
+  url.query = nil
+
+  this.signature_base_string = sequence_writer()
+    :write(request.method:upper())
+    :write("&")
+    :write(uri.encode(url))
+    :write("&")
+    :write(uri.encode(this.parameter_string))
+    :concat()
+  return self
+end
+
+function class:make_signature(request, oauth_consumer_secret, oauth_token_secret)
+  local this = request.oauth
+  local signing_key = uri.encode(oauth_consumer_secret) .. "&" .. uri.encode(oauth_token_secret)
+  this.signature = base64.encode(sha1.hmac(signing_key, this.signature_base_string, "bin"))
+  return self
+end
+
+function class:make_header(request)
+  local this = request.oauth
+  local oauth_params = sequence()
+    :push({ "oauth_consumer_key", uri.encode(self.oauth_consumer_key) })
+    :push({ "oauth_nonce", uri.encode(self.oauth_nonce) })
+    :push({ "oauth_signature", uri.encode(this.signature) })
+    :push({ "oauth_signature_method", "HMAC-SHA1" })
+    :push({ "oauth_timestamp", uri.encode(self.oauth_timestamp) })
+    :push({ "oauth_token", uri.encode(self.oauth_token) })
+    :push({ "oauth_version", "1.0" })
+
+  local out = sequence_writer():write("OAuth ")
+  local first = true
+  for param in oauth_params:each() do
+    if first then
+      first = false
+    else
+      out:write(", ")
+    end
+    out:write(param[1], "=\"", param[2], "\"")
+  end
+
+  local authorization = out:concat()
+  this.authorization = authorization
+  request:header("Authorization", authorization)
+  return self
+end
+
 local metatable = {
   __index = class;
 }
 
 return setmetatable(class, {
-  __call = function (_)
-    return setmetatable(class.new(), metatable)
+  __call = function (_, oauth_consumer_key, oauth_token)
+    return setmetatable(class.new(oauth_consumer_key, oauth_token), metatable)
   end;
 })
--- test/test_twitter.lua
+++ test/test_twitter.lua
@@ -15,11 +15,6 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-http.  If not, see <http://www.gnu.org/licenses/>.
 
-local base64 = require "dromozoa.commons.base64"
-local json = require "dromozoa.commons.json"
-local sequence = require "dromozoa.commons.sequence"
-local sequence_writer = require "dromozoa.commons.sequence_writer"
-local sha1 = require "dromozoa.commons.sha1"
 local http = require "dromozoa.http"
 local oauth = require "dromozoa.http.oauth"
 
@@ -27,69 +22,24 @@ local ua = http.user_agent():fail():verbose()
 
 local scheme = "https"
 local host = "api.twitter.com"
-local consumer_secret = "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw"
-local oauth_token_secret = "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE"
 
 local uri = http.uri(scheme, host, "/1/statuses/update.json")
   :param("include_entities", "true")
 local request = http.request("POST", uri)
-  :encode(http.uri.encode)
   :param("status", "Hello Ladies + Gentlemen, a signed OAuth request!")
-request:build()
-assert(request.content == [[
-status=Hello%20Ladies%20%2B%20Gentlemen%2C%20a%20signed%20OAuth%20request%21]])
 
-local params = sequence()
-params:push({ "oauth_consumer_key", "xvz1evFS4wEEPTGEFPHBog" })
-params:push({ "oauth_nonce", "kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg" })
-params:push({ "oauth_signature_method", "HMAC-SHA1" })
-params:push({ "oauth_timestamp", "1318622958" })
-params:push({ "oauth_token", "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb" })
-params:push({ "oauth_version", "1.0" })
-for param in request.uri.query.params:each() do
-  params:push({ param[1], param[2] })
-end
-for param in request.params:each() do
-  params:push({ param[1], param[2] })
-end
-
-for param in params:each() do
-  param[1] = http.uri.encode(param[1])
-  param[2] = http.uri.encode(param[2])
-end
-params:sort(function (a, b) return a[1] < b[1] end)
-
-local out = sequence_writer()
-local first = true
-for param in params:each() do
-  if first then
-    first = false
-  else
-    out:write("&")
-  end
-  out:write(param[1], "=", param[2])
-end
-local parameter_string = out:concat()
-assert(parameter_string == [[
+local a = oauth("xvz1evFS4wEEPTGEFPHBog", "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb")
+    :reset(1318622958, "kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg")
+    :build(request)
+    :make_parameter_string(request)
+    :make_signature_base_string(request)
+    :make_signature(request, "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw", "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE")
+    :make_header(request)
+assert(request.oauth.parameter_string == [[
 include_entities=true&oauth_consumer_key=xvz1evFS4wEEPTGEFPHBog&oauth_nonce=kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1318622958&oauth_token=370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb&oauth_version=1.0&status=Hello%20Ladies%20%2B%20Gentlemen%2C%20a%20signed%20OAuth%20request%21]])
-
-local signature_base_string
-    = request.method:upper()
-    .. "&"
-    .. http.uri.encode(request.uri.scheme .. "://" .. request.uri.authority .. request.uri.path)
-    .. "&"
-    .. http.uri.encode(parameter_string)
-assert(signature_base_string == [[
+assert(request.oauth.signature_base_string == [[
 POST&https%3A%2F%2Fapi.twitter.com%2F1%2Fstatuses%2Fupdate.json&include_entities%3Dtrue%26oauth_consumer_key%3Dxvz1evFS4wEEPTGEFPHBog%26oauth_nonce%3DkYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1318622958%26oauth_token%3D370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb%26oauth_version%3D1.0%26status%3DHello%2520Ladies%2520%252B%2520Gentlemen%252C%2520a%2520signed%2520OAuth%2520request%2521]])
-
-local signing_key = http.uri.encode(consumer_secret) .. "&" .. http.uri.encode(oauth_token_secret)
-assert(signing_key == [[
-kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw&LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE]])
-
-local oauth_signature = base64.encode(sha1.hmac(signing_key, signature_base_string, "bin"))
-assert(oauth_signature == [[
+assert(request.oauth.signature == [[
 tnnArxj06cWHq44gCs1OSKk/jLY=]])
-
-local a = oauth():reset()
--- print(json.encode(a))
-print(a.oauth_nonce)
+assert(request.oauth.authorization == [[
+OAuth oauth_consumer_key="xvz1evFS4wEEPTGEFPHBog", oauth_nonce="kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg", oauth_signature="tnnArxj06cWHq44gCs1OSKk%2FjLY%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1318622958", oauth_token="370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb", oauth_version="1.0"]])
